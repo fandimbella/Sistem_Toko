4363b6a2c860f8f82a7d48c284c10b38810c44a7
fandhimbella <fandimbella@gmail.com>
2026-07-17 11:38:24 +0700

    Project UAS PBO Sistem Toko

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:sistem_toko/sistem_toko.dart' as sistem_toko;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sistem_toko.calculate()}!');
+}
---
+++ test/sistem_toko_test.dart
@@ -0,0 +1,8 @@
+import 'package:sistem_toko/sistem_toko.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
